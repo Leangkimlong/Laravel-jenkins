e4796bb7be70d3bda2fa2d7559b45da8b01ae3e9
Leangkimlong <mizterlong95@gmail.com>
2024-03-29 01:57:08 +0700

    commit

---
+++ src/main/java/i4/jenkins/i4jenkins/I4jenkinsApplication.java
@@ -0,0 +1,13 @@
+package i4.jenkins.i4jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class I4jenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(I4jenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/i4/jenkins/i4jenkins/I4jenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package i4.jenkins.i4jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class I4jenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
